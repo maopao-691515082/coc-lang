ef11e4514ad7d1983a4afcbd89c232d02c94936f
maopao-691515082 <691515082@qq.com>
2020-01-23 00:05:55 +0800

    update

--- lib/time.coc_native_mod.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIME_COC_NATIVE_MOD_H
-#define TIME_COC_NATIVE_MOD_H
-
-#include "util.coc.h"
-
-coc_long_t time_$_func_time_us();
-CocPtr<__builtins_$_cls_String> time_$_func_strftime(__builtins_$_cls_String *fmt, coc_long_t tm);
-
-#endif
